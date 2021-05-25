be23e30aed70e800b54e7f3d9b2f83d78244027d
leoita <leonardolorusso83@gmail.com>
2021-05-25 21:54:37 +0200

    PATCH - update only needed product parameters

--- src/main/java/com/leoita/springBootRestApi/controller/ProductController.java
+++ src/main/java/com/leoita/springBootRestApi/controller/ProductController.java
@@ -4,9 +4,12 @@ import com.leoita.springBootRestApi.model.Product;
 import com.leoita.springBootRestApi.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/product")
@@ -37,6 +40,21 @@ public class ProductController {
     //update a product
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
     public Product updateProduct(@RequestBody Product product) {
+        productServiceImpl.updateProduct(product);
+        return product;
+    }
+
+    //update only needed product parameters
+    @PatchMapping("/{id}")
+    public Product updateProductParameter(@PathVariable("id") Long Id, @RequestBody Map<String, Object> fields) {
+        Product product = productServiceImpl.getProductById(Id);
+        fields.forEach((key, value) -> {
+            Field field = ReflectionUtils.findField(Product.class, key); //find the field with name equal to the key
+            if (field != null) {
+                field.setAccessible(true); // allow the edition of the field
+                ReflectionUtils.setField(field, product, value); //set a value to the field
+            }
+        });
         productServiceImpl.updateProduct(product);
         return product;
     }
